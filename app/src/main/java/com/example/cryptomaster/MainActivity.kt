e56253ed2f3eef6f695561abf3dddc3ace0274ba
Jay <jayraj1999@gmail.com>
2022-01-19 10:04:34 +0530

    viewBinding Added

--- app/src/main/java/com/example/cryptomaster/MainActivity.kt
+++ app/src/main/java/com/example/cryptomaster/MainActivity.kt
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import com.example.cryptomaster.databinding.ActivityMainBinding
 import com.example.cryptomaster.retrofit.BuilderInstance
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.tvMain.text = "haha asd"
 
         lifecycleScope.launchWhenCreated {
             val response = try {
